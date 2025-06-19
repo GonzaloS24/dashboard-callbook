5d2f9ceb106f25deef0a0b213fd73930a8ad9992
GonzaloS24 <gonzalo.salazar@chateapro.com>
2025-06-19 17:39:42 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/navbar/Navbar";
+import Dashboard from "./pages/dashboard/Dashboard";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Dashboard />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import Button from "../../components/ui/button";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold text-center my-4">Dashboard</h1>
+      <Button />
+    </div>
+  );
+};
+
+export default Dashboard;
